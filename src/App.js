d1101f22019d76d72569ae23aa4c0c9e2695f9f7
Max <57855468+musaCODEzz@users.noreply.github.com>
2022-04-17 16:57:21 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,86 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState, useEffect } from 'react';
+import { Button, FormControl, InputLabel, Input  } from '@mui/material';
+import Todo from './Todo';
+import { db } from './FirebaseConfig';
+import {
+  collection, onSnapshot, addDoc, serverTimestamp, orderBy, query
+} from 'firebase/firestore';
+
+
+
+
 
 function App() {
+
+
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState('');
+  
+
+
+
+  // as the app loads we neeed to listen to the database and fetch new todos as they get added
+
+  useEffect(() => {
+   /// the code fired when app.js loads
+
+   const colRef = collection(db, 'todos');
+   const q = query(colRef, orderBy("timestamp", "asc"));
+
+    // listen to the collection and fetch new todos as they get added to the database
+
+   onSnapshot(q, snapshot => {
+      //console.log(snapshot.docs);
+      setTodos(snapshot.docs.map(doc =>({id:doc.id, todo: doc.data().todo}) ))
+   })
+
+  }, []);
+
+
+  const addTodo = (event) => {
+    //console.log("The button is working");
+    event.preventDefault();
+
+    const colRef = collection(db, 'todos');
+   
+
+    // colRef.addDoc({
+    //   todo: input
+    // })
+    addDoc(colRef, {
+        todo: input,
+        timestamp: serverTimestamp()
+     })
+    
+
+    setTodos([...todos, input]);
+    setInput('');
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Creating a TODO List</h1>
+      <form>
+            {/* <input type= "text"  value={input} onChange={e => setInput(e.target.value)}/> */}
+            <FormControl>
+                  <InputLabel >Write a TODO</InputLabel>
+                  <Input value={input} onChange={e => setInput(e.target.value)} />
+                  
+            </FormControl>
+            <Button disabled={!input} variant="outlined"  type="submit"onClick={addTodo} >Add Task</Button>
+            {/* <button type="submit" onClick={addTodo}>Add Task</button> */}
+      </form>
+
+      
+      <ul>
+        {todos.map(todo => (
+          // <li>{todo}</li>
+          <Todo todo={todo} />
+        ))}
+        
+      </ul>
+      
     </div>
   );
 }
---
+++ src/Todo.js
@@ -0,0 +1,25 @@
+import { List, ListItem, ListItemText } from '@mui/material';
+import React from 'react';
+import { db } from './FirebaseConfig';
+import { 
+   deleteDoc, doc
+} from 'firebase/firestore';
+import { DeleteRounded } from '@mui/icons-material';
+
+const Todo = (props) => {
+  return (
+    <div>
+        <List className='todo-list'>
+            <ListItem>
+                <ListItemText primary={props.todo.todo} secondary={props.text} />
+            </ListItem>
+            <DeleteRounded onClick={event => deleteDoc(doc(db, 'todos', props.todo.id))} />
+            {/* <Button onClick={event => deleteDoc(doc(db, 'todos', props.todo.id))}>Delete Me</Button> */}
+        </List>
+        
+    </div>
+    
+  )
+}
+
+export default Todo
