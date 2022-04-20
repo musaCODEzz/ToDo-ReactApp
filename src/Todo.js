9060e07454e6bd429f9d3d21b160822f521c203f
Max <57855468+musaCODEzz@users.noreply.github.com>
2022-04-20 12:11:32 +0300

    commit on master

--- src/Todo.js
+++ src/Todo.js
@@ -5,6 +5,7 @@ import {
    deleteDoc, doc
 } from 'firebase/firestore';
 import { DeleteRounded } from '@mui/icons-material';
+import './index.css'
 
 const Todo = (props) => {
   return (
@@ -13,7 +14,7 @@ const Todo = (props) => {
             <ListItem >
                 <ListItemText  primary={props.todo.todo} secondary={props.text} />
             </ListItem>
-            <DeleteRounded onClick={event => deleteDoc(doc(db, 'todos', props.todo.id))} />
+            <DeleteRounded  id="delete" onClick={event => deleteDoc(doc(db, 'todos', props.todo.id))} />
             {/* <Button onClick={event => deleteDoc(doc(db, 'todos', props.todo.id))}>Delete Me</Button> */}
         </List>
         
